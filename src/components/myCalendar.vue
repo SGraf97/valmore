21c55aa013cbfcbdbaa7ea07b3b9b2db1d349106
Spyros Stafylarakis <csd3722@csd.uoc.gr>
2022-01-15 19:49:33 +0200

    fetching events

--- src/components/myCalendar.vue
+++ src/components/myCalendar.vue
@@ -1,76 +1,71 @@
 <template>
-<div>
-  
-  <button v-on:click="alertShow=false">test</button>
-  <vue-cal
-    :time-from="10 * 60"
-    :time-to="23 * 60"
-    :disable-views="['years', 'year']"
-    :events="events"
-    :editable-events="{
-      title: true,
-      drag: true,
-      resize: true,
-      delete: true,
-      create: true,
-    }"
-    :on-event-create="onEventCreate"
-    class="vuecal--full-height-delete vuecal--green-theme "
-    today-button
-    :snap-to-time="15"
-  >
-  <template v-slot:today-button>
-    <v-tooltip>
-      <template v-slot:activator="{ on }">
-        <v-btn v-on="on">
-          <p>today</p>
-        </v-btn>
+  <div>
+    <vue-cal
+      style="height: 1000px"
+      :time="false"
+      :disable-views="['years', 'year']"
+      :events="events"
+      editable-events
+      :on-event-create="onEventCreate"
+      class="vuecal--green-theme"
+      today-button
+      :snap-to-time="15"
+      :drag-to-create-event="false"
+      
+    >
+      <template v-slot:today-button>
+        <v-tooltip>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on">
+              <p>today</p>
+            </v-btn>
+          </template>
+        </v-tooltip>
       </template>
-    </v-tooltip>
-  </template>
-  </vue-cal>
+    </vue-cal>
 
-  <v-dialog v-model="showEventCreationDialog" :persistent="true" max-width="420" >
-    <v-card>
-      <v-card-title>
-        <v-text-field v-model="selectedEvent.title" placeholder="Event Title"/>
-      </v-card-title>
-      <v-card-text>
-        <v-textarea v-model="selectedEvent.description" placeholder="Event Content"/>
-        <v-layout>
-          <v-select
-            :items="eventsCssClasses"
-            placeholder="Event CSS Class"
-            @change="selectedEvent.class = $event"
-            :value="selectedEvent.class"/>
-          <v-switch v-model="selectedEvent.background" label="background Event"/>
-        </v-layout>
-        <v-layout>
-          <v-btn @click="cancelEventCreation()">Cancel</v-btn>
-          <v-btn @click="closeCreationDialog()">Save</v-btn>
-        </v-layout>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
-  <v-alert
-    class="alert"
-    :type="alert.type"
-    :value="alertShow"
-    width= "500px"
-    :class="alert.type+'-alert'"
-    transition="scale-transition"
-  >
-    {{alert.message}}
-
-  </v-alert>
-</div>
-
-</template> 
+    <v-dialog
+      v-model="showEventCreationDialog"
+      :persistent="true"
+      max-width="420"
+    >
+      <v-card>
+        <v-card-title>
+          <v-text-field
+            v-model="selectedEvent.title"
+            placeholder="Event Title"
+          />
+        </v-card-title>
+        <v-card-text>
+          <v-textarea
+            v-model="selectedEvent.description"
+            placeholder="Event Content"
+          />
+          <v-layout>
+            <v-btn @click="cancelEventCreation()">Cancel</v-btn>
+            <v-btn @click="closeCreationDialog()">Save</v-btn>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    <v-alert
+      class="alert"
+      :type="alert.type"
+      :value="alertShow"
+      width="500px"
+      :class="alert.type + '-alert'"
+      transition="scale-transition"
+    >
+      {{ alert.message }}
+    </v-alert>
+  </div>
+</template>
 <script>
-import 'vue-cal/dist/drag-and-drop.js'
+import "vue-cal/dist/drag-and-drop.js";
 import VueCal from "vue-cal";
 import "vue-cal/dist/vuecal.css";
 import api from "../configs";
+
 
 export default {
   name: "myCalendar",
@@ -84,29 +79,60 @@ export default {
       loadReady: false,
       c: new api(),
       days: [],
-      events: [],
-      eventsCssClasses: ['leisure', 'sport', 'health'],
+      events: [
+        {
+          start: '2022-01-12 10:00:00',
+          end: '2022-01-12 10:30:00',
+          title: '1',
+          content: '1',
+          class: 'primary'
+        },
+        {
+          start: '2022-01-12 10:00:00',
+          end: '2022-01-12 10:45:00',
+          title: '2',
+          content: '2',
+          class: 'info'
+        },
+        {
+          start: '2022-01-12 10:00:00',
+          end: '2022-01-12 11:00:00',
+          title: '3',
+          content: '3',
+          class: 'warning'
+        },
+        {
+          start: '2022-01-12 09:59:59',
+          end: '2022-01-12 11:00:01',
+          title: '4',
+          content: '4',
+          class: 'danger'
+        }
+      ],
+      eventsCssClasses: ["leisure", "sport", "health"],
       selectedDate: new Date(new Date().getFullYear(), 11, 31),
-      alert:{
-        message: '',
-        type: '',
+      alert: {
+        message: "",
+        type: "",
       },
-      alertShow:false,
+      alertShow: false,
     };
   },
   computed: {},
   methods: {
-    onEventCreate(event , deleteEventFunction) {
+    onEventCreate(event, deleteEventFunction) {
       this.selectedEvent = event;
       this.showEventCreationDialog = true;
       this.deleteEventFunction = deleteEventFunction;
       console.log(event);
       return event;
     },
+
     cancelEventCreation() {
-      this.closeCreationDialog();
+      this.showEventCreationDialog = false;
       this.deleteEventFunction();
     },
+
     closeCreationDialog() {
       this.showEventCreationDialog = false;
       console.log(this.selectedEvent);
@@ -114,56 +140,86 @@ export default {
         event_name: this.selectedEvent.title,
         event_description: this.selectedEvent.description,
         event_date: this.selectedEvent.start,
-        // created_by: 
+        // created_by:
         created_at: Date.now(),
-        updated_at: '',
-        deleted_at: ''
-      }
-      this.axios.post(`${this.c.createEvent}` , eventToUpload , {headers: this.c.headers}).then(res=>{
-        console.log(res);
-      }).catch(error =>{
-        console.log(error);
-      })
+        updated_at: "",
+        deleted_at: "",
+      };
+      this.axios
+        .post(`${this.c.createEvent}`, eventToUpload, {
+          headers: this.c.headers,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.data.success) {
+            this.showAlert({ message: res.data.message, type: "success" });
+          } else {
+            this.showAlert({ message: res.data.message, type: "error" });
+            this.deleteEventFunction();
+          }
+        })
+        .catch((error) => {
+          this.showAlert({ message: error.message, type: "error" });
+          console.log(error);
+        });
       this.selectedEvent = {};
     },
-    showAlert(alert){
+
+    convertEvent(event){
+      var returnedEvent = {
+        class : 'primary',
+        start : event.created_at.split('T')[0] + ' ' + event.created_at.split('T')[1].split('.')[0],
+        end : event.created_at.split('T')[0] + ' ' + event.created_at.split('T')[1].split('.')[0],
+        title: event.event_name,
+        content: event.event_description,
+      }
+      return returnedEvent;
+    },
+
+    showAlert(alert) {
       this.alert = alert;
       this.alertShow = true;
-      setTimeout(()=>{this.alertShow = false} , 5000);
-    }
+      setTimeout(() => {
+        this.alertShow = false;
+      }, 5000);
+    },
   },
+
   created: function () {
     console.log(this.c.createEvent);
-    this.showAlert({message:'this works' , type:'info'});
     this.axios
       .post(`${this.c.listURL}`, {}, { headers: this.c.headers })
       .then((res) => {
         console.log(res);
-        if(res.data.success){
-          this.events = res.data.return.docs;
+        if (res.data.success) {
+          console.log(res.data);
+          for (var i of res.data.return.docs){
+            this.events.push(this.convertEvent(i));
+            console.log(this.events);
+          }
+          // this.events = res.data.return.docs;
         }
       });
   },
 };
 </script>
 <style scoped>
-.alert{
+.alert {
   position: fixed;
   bottom: 0;
-  right:50px;
+  right: 50px;
 }
-.error-alert{
+.error-alert {
   background-color: red !important;
-  color: black !important;
+  color: white !important;
 }
 
-.info-alert{
+.info-alert {
   background-color: indigo !important;
   color: white !important;
-
 }
 
-.success-alert{
+.success-alert {
   background-color: #42b983 !important ;
   color: white !important;
 }
